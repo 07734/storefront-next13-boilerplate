e78eacd680327538f36a5dc6eecf37d68e400c72
Alistair Bancroft <hello@0.7734.co.uk>
2023-07-06 11:12:54 +0100

    Initial commit

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['next/core-web-vitals', '@vue-storefront/eslint-config', '@vue-storefront/eslint-config/react'],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    'max-len': ['warn', { code: 120, ignoreStrings: true, ignoreUrls: true, ignoreTemplateLiterals: true }],
+    'max-lines-per-function': 'off',
+    'unicorn/no-keyword-prefix': ['error', { disallowedPrefixes: ['new', 'for'] }],
+    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+    complexity: 0,
+    'no-secrets/no-secrets': 'off',
+    'unicorn/prefer-array-some': 'off',
+    'sonarjs/no-duplicate-string': 'off',
+    'max-statements': 'off',
+  },
+};
